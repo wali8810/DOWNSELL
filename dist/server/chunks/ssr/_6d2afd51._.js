e582aa53e1fe1dbf006f9e92cfc8e27e9e8eed6a
Walisson Conta pessoal <walissoncontapessoal@gmail.com>
2025-09-07 19:33:13 +0000

    cor das resposta coloca branca (_for element <Primitive.div>_)

--- dist/server/chunks/ssr/_6d2afd51._.js
+++ dist/server/chunks/ssr/_6d2afd51._.js
@@ -1960,7 +1960,7 @@ function Faq() {
                                     columnNumber: 15
                                 }, this),
                                 /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$src$2f$components$2f$ui$2f$accordion$2e$tsx__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["AccordionContent"], {
-                                    className: "text-yellow-200/80 text-base px-4",
+                                    className: "text-white/80 text-base px-4",
                                     children: item.answer
                                 }, void 0, false, {
                                     fileName: "[project]/src/components/landing/Faq.tsx",
--- src/components/landing/Faq.tsx
+++ src/components/landing/Faq.tsx
@@ -37,7 +37,7 @@ export function Faq() {
           {faqItems.map((item, index) => (
             <AccordionItem value={`item-${index}`} key={index} className={index === faqItems.length - 1 ? "border-b-0" : "border-b-yellow-400/30"}>
               <AccordionTrigger className="text-lg text-left hover:no-underline px-4">{item.question}</AccordionTrigger>
-              <AccordionContent className="text-yellow-200/80 text-base px-4">
+              <AccordionContent className="text-white/80 text-base px-4">
                 {item.answer}
               </AccordionContent>
             </AccordionItem>
